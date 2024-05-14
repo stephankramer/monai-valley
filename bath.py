c4220042707ef20d6bba77decdc0a7157d4a6359
Stephan Kramer <s.kramer@imperial.ac.uk>
2024-05-14 11:03:30 +0100

    Change to new interpolator

--- bath.py
+++ bath.py
@@ -5,7 +5,8 @@ import model_config
 mesh = RectangleMesh(50, 30, 5.488, 3.402)
 V = FunctionSpace(mesh, "CG", 1)
 bathymetry2d = Function(V, name='bathymetry2d')
-model_config.interpolate_bathymetry(bathymetry2d)
+bathymetry_interpolator = model_config.BathymetryInterpolator(bathymetry2d)
+bathymetry_interpolator.interpolate()
 File('bathymetry.pvd').write(bathymetry2d)
 with CheckpointFile('monai_bathymetry_F.h5', 'w') as afile:
     afile.save_mesh(mesh)
